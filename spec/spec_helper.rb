0a9c6a365a0f65b80fa1466de94f9f45177ecde1
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-08-21 17:55:16 +0200

    Add coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,27 @@
-require "bundler/setup"
-require "game_of_life"
+# frozen_string_literal: true
+
+if ENV['COVERALLS']
+  require 'coveralls'
+  Coveralls.wear!
+end
+
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start
+end
+
+require 'bundler/setup'
+require 'game_of_life'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.order = :random
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
